9bbf929c9cfaa60d3067e50734194b82ccba2072
yashasapp925 <yashas.appaji@gmail.com>
2024-09-08 23:09:47 -0400

    Changing blast type and match criteria

--- find_homologs.sh
+++ find_homologs.sh
@@ -5,8 +5,8 @@ subject_file="$2"
 output_file="$3"
 
 query_length=$(awk 'NR==2 {print length($0)}' $query_file)
-blastn -query "$query_file" -subject "$subject_file" -outfmt "6 qseqid sseqid pident length" -out temp.txt
-awk -v x=$query_length '$3 == 100 && $4 == x' temp.txt | awk '{print $0}' > "$output_file"
+tblastn -query "$query_file" -subject "$subject_file" -outfmt "6 qseqid sseqid pident length" -out temp.txt
+awk -v x=$query_length '$3 > 30 && $4 >= 0.9 * x' temp.txt > "$output_file"
 num_matches=$(wc -l < "$output_file")
-echo "Number of perfect matches: $num_matches"
+echo "Number of matches: $num_matches"
 rm temp.txt
