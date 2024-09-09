bad4d85786da7c3aa4d420543d89e90af4d6e3a1
yashasapp925 <yashas.appaji@gmail.com>
2024-09-09 14:26:06 -0400

    Code change for finding matches

--- find_homologs.sh
+++ find_homologs.sh
@@ -2,9 +2,8 @@ query_file="$1"
 subject_file="$2"
 output_file="$3"
 
-query_length=$(awk 'NR==2 {print length($0)}' $query_file)
-tblastn -query "$query_file" -subject "$subject_file" -outfmt "6 qseqid sseqid pident length" -out temp.txt
-awk -v x=$query_length '$3 > 30 && $4 >= 0.9 * x' temp.txt > "$output_file"
+tblastn -query "$query_file" -subject "$subject_file" -outfmt "6 qseqid sseqid pident length qlen" -out temp.txt
+awk -v x=$query_length '$3 > 30 && $4 >= 0.9 * $5' temp.txt > "$output_file"
 num_matches=$(wc -l < "$output_file")
 echo "Number of matches: $num_matches"
 rm temp.txt
